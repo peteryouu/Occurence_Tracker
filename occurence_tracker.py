16459d66a0c082d378ba6babef5983623260f6e8
peteryouu <48395977+peteryouu@users.noreply.github.com>
2019-07-09 23:35:04 -0400

    Update occurence_tracker.py
    Added some libraries and packages needed for future code, and added in lists being used for numpy

--- occurence_tracker.py
+++ occurence_tracker.py
@@ -1,12 +1,26 @@
 import csv
-x = input('File name:')
-occurence = {}
-with open(x,'r') as fh:
-    dates = csv.DictReader(fh)
-    for row in dates:
-        if row['Dates'] not in occurence:
-            occurence[row['Dates']] = 1
-        else:
-            occurence[row['Dates']] += 1
+import matplotlib as plt
+import numpy
+from sklearn import linear_model
 
-    print(occurence)
+
+x = input('File name:')
+
+
+occurence = {}
+
+def file_open(file_name):
+    with open(file_name,'r') as fh:
+        dates = csv.DictReader(fh)
+        for row in dates:
+            if row['Dates'] not in occurence:
+                occurence[row['Dates']] = 1
+            else:
+                occurence[row['Dates']] += 1
+
+
+file_open(x)
+
+
+o_keys = list(occurence.keys())
+o_values = list(occurence.values())
